acfed6e09d4d82931ab098ccdf1b99ce844be1b5
aprajita_jain <aprajitajain90@gmail.com>
2016-12-19 13:23:01 -0800

    completed 6ees6ees6ees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+	constructor(color ='yellow',food){
+		super(color ='yellow',food)
+		this.age = 10;
+		this.job = 'find pollen';
+		this.canFly = true;
+		this.treasureChest = [];
+	}
+	forage(){
+		this.treasureChest.push('treasure');
+	}
+
+ 
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+	constructor(color='yellow',food){
+		super(color='yellow',food)
+		this.age = 10;
+		this.job='make honey';
+		this.honeyPot = 0;
+	}
+	makeHoney(){
+		this.honeyPot++;
+	}
+	giveHoney(){
+		this.honeyPot--;
+	}
+  
 };
